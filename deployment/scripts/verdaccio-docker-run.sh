c9e31dc8ee21d08674ac74b3809e658afc624ff1
Arif Ginanjar <arif.ginanjar@agate.id>
2020-05-04 00:49:48 +0700

    fixing wrong mounting volume

--- deployment/scripts/verdaccio-docker-run.sh
+++ deployment/scripts/verdaccio-docker-run.sh
@@ -1,7 +1,12 @@
 DOC_PROJ_NAME=[DOC_PROJ_NAME]
 DOC_IMAGE_NAME=[DOC_IMAGE_NAME]
 DOC_IMAGE_TAG=[DOC_IMAGE_TAG]
-DOC_MNT_VOL=/home/[SERVER_USER_NAME]/
+DOC_MNT_VOL=/home/[SERVER_USER_NAME]/verdaccio
+DOC_MNT_VOL_CONF=/conf
+DOC_MNT_VOL_LOG=/log
+DOC_MNT_VOL_STOR=/storage
+DOC_MNT_VOL_WEB=/web
+DOC_MNT_VOL_PLUG=/plugins
 DOC_PUBISH_PORT=[DOC_PUBISH_PORT]
 PACKAGE_IMAGE_NAME=[PACKAGE_IMAGE_NAME]
 DOC_CUST_CMD="[DOC_CUST_CMD]"
@@ -15,7 +20,11 @@ set -e
 docker load --input ${PACKAGE_IMAGE_NAME}.tar
 
 docker run -d --name ${DOC_PROJ_NAME} \
-  -v ${DOC_MNT_VOL}:/verdaccio \
+  -v ${DOC_MNT_VOL}${DOC_MNT_VOL_CONF}:/verdaccio/conf \
+  -v ${DOC_MNT_VOL}${DOC_MNT_VOL_LOG}:/verdaccio/log \
+  -v ${DOC_MNT_VOL}${DOC_MNT_VOL_PLUG}:/verdaccio/plugins \
+  -v ${DOC_MNT_VOL}${DOC_MNT_VOL_STOR}:/verdaccio/storage \
+  -v ${DOC_MNT_VOL}${DOC_MNT_VOL_WEB}:/verdaccio/web \
   -p ${DOC_PUBISH_PORT}:4873 \
   --restart unless-stopped \
   ${DOC_CUST_CMD} ${DOC_IMAGE_NAME}:${DOC_IMAGE_TAG}
